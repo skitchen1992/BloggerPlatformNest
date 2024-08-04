b8df32fbcfe1284c023e13f1ce3513fee4fac866
nikita <nikita.lavrentyev@mysky.com>
2024-08-04 22:34:49 +0400

    fix APISettings

--- src/settings/api-settings.ts
+++ src/settings/api-settings.ts
@@ -1,10 +1,11 @@
 import { EnvironmentVariable } from '@settings/configuration';
-import { IsEmail, IsNumber, IsString } from 'class-validator';
+import { IsEmail, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class APISettings {
   constructor(private readonly envVariables: EnvironmentVariable) {}
   // Application
   @IsNumber()
+  @IsOptional()
   public readonly APP_PORT: number = Number(this.envVariables.APP_PORT);
 
   // Database
